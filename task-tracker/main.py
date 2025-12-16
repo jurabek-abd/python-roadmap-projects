a62d8477eeecc83585127ff25862295e72f3f95d
jurabek-abd <jurabek.pro.abd@gmail.com>
2025-12-16 19:14:11 +0500

    Two command parsers added to Task Tracker

--- task-tracker/main.py
+++ task-tracker/main.py
@@ -1,1 +1,20 @@
-print("Work in Progress!")
+import argparse
+
+parser = argparse.ArgumentParser(description="Task Tracker CLI")
+subparsers = parser.add_subparsers(dest="command", required=True)
+
+# Adding a new task
+add_parser = subparsers.add_parser("add", help="Add a new task")
+add_parser.add_argument("task", type=str, help="Task value")
+
+# Updating a task by ID
+update_parser = subparsers.add_parser("update", help="Update a task")
+update_parser.add_argument("id", type=int, help="Task ID")
+update_parser.add_argument("task", type=str, help="Task value")
+
+args = parser.parse_args()
+
+if args.command == "add":
+    print(f"Added a task: {args.task}")
+elif args.command == "update":
+    print(f"Updated a task {args.id}: {args.task}")
